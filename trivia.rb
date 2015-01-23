a4897576d38a3f426d956998e2be330975d85c0a
Josh Alexy <jalexy12@gmail.com>
2015-01-23 16:36:38 -0500

    second commit of IMDB app

--- trivia.rb
+++ trivia.rb
@@ -7,9 +7,20 @@ enable :sessions
 set :session_secret, 'movie_secrets'
 
 
+def how_many_posters(movies)
+	movie_list = []
+
+	movies.each do |i|
+		if movie_list.length < 9 && i.poster != nil
+			movie_list.push(i)
+		end
+	end
+	movie_list
+end
+
 # search = Imdb::Search.new('rashamon')
 
-# ap search.movies
+# ap search.moviese
 
 
 get '/' do 
@@ -21,16 +32,17 @@ get '/search' do
 	erb :search
 end
 
+
+
+
 post "/search_movies" do
-	
 	@search = Imdb::Search.new(params[:movie_term])
 	@search_results = @search.movies
-	@top_nine = @search_results[0..8]
-	erb :search_results
-	 
-
-	
+	@top_movies = @search_results[0..20]
+	@clean_list =  how_many_posters(@top_movies)
+	erb :search_results	
 end
+
 
  # get "/movie_results" do
 	# 
